b66b7a435a3d9f425736834e525f67ce3277344c
Dan Finneran <daniel.finneran@gmail.com>
2018-06-05 11:12:15 +0100

    Docker QR

---
+++ Readme.md
@@ -0,0 +1,8 @@
+
+# dockqr
+
+## Please complete heading
+
+Additional text
+
+Created on 2018-06-05 11:11:58
---
+++ hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Printf("Hello World for your new project created\n")
+
+}
